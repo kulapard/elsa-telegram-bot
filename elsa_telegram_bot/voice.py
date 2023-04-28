8164ed9cf7dbc344cf9c73323f6874f886a16dd8
Taras Drapalyuk <taras@drapalyuk.com>
2023-04-28 15:15:19 +0400

    use api_key inside Audio (#13)

--- elsa_telegram_bot/voice.py
+++ elsa_telegram_bot/voice.py
@@ -8,10 +8,6 @@ from elsa_telegram_bot.config import OPENAI_API_TOKEN
 OGG_EXTENSION = ".oga"
 MP3_EXTENSION = ".mp3"
 
-# TODO: fix api_key in openai.Audio
-#       https://github.com/openai/openai-python/issues/407
-openai.api_key = OPENAI_API_TOKEN
-
 
 async def convert_ogg_to_mp3(ogg_file: str, mp3_file: str):
     audio = AudioSegment.from_file_using_temporary_files(ogg_file, "ogg")
@@ -21,5 +17,7 @@ async def convert_ogg_to_mp3(ogg_file: str, mp3_file: str):
 async def transcribe_audio(mp3_file: AsyncBufferedReader) -> str:
     logger.info(f"Transcribing {mp3_file}")
     with open(mp3_file.name, "rb") as f:
-        result = await openai.Audio().atranscribe(file=f, model="whisper-1")
+        result = await openai.Audio.atranscribe(
+            file=f, model="whisper-1", api_key=OPENAI_API_TOKEN
+        )
     return result["text"]  # type: ignore[no-any-return]
